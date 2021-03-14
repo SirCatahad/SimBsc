613fb548b968ae10078c893b6bc2c7385ca4da40
Nekomancer <Wufftiger@gmail.com>
2021-03-14 12:44:44 +0100

    Updated makeMissing

--- sim_functions.R
+++ sim_functions.R
@@ -31,18 +31,10 @@ simData <- function(n, r.sqrd, coefficients ,parameters)
 # data       - the data frame which should get missing observations
 # mechanism  - the mechanism of missing data, by default MCAR
 # percent    - the proportion of observations that should be set to missing (NA)
-# indexRange - A vector of indices restricting which columns should contain missing values, by default everything except the last.
-makeMissing <- function(data, mechanism="MCAR", percent, indexRange=-length(data))
+# indexRange - A vector of indices restricting which columns should contain missing values
+makeMissing <- function(data, mechanism="MCAR", percent, indices)
 {
-  #assign the columns that should contain missing data
-  #drop=FALSE prevents the drop in dimension if  the number of columns is reduced to 1
-  df <- data[,indexRange, drop=FALSE]
-  #size of the new data.frame
-  size <- ncol(df) * nrow(df)
-  #the amount of observations that need to be deleted in order to obtain the set percentage.
-  to_delete <- size*percent
-  
-  
+  df <- data
   if(mechanism=="MAR")
   {
     
@@ -51,40 +43,18 @@ makeMissing <- function(data, mechanism="MCAR", percent, indexRange=-length(data
   #MCAR missing data mechanism
   if(mechanism=="MCAR")
   {
-    #create a matrix containing row and column indices
-    indices <- as.matrix(expand.grid(1:nrow(df), 1:ncol(df)))
     
-    #randomly select indices until we have enough
-    selected <- indices[sample(nrow(indices), to_delete), ]
-    
-    #set selected observations to missing
-    df[selected] <- NA
-    
-    #replace the columns in the original data
-    df <- replaceColumns(data, df)
-    
+    #delete the specified amount of cases in each of the specified columns
+    for(i in indices)
+    {
+      df[sample(1:nrow(data), nrow(df)*percent) , i] <- NA
+    }
+
     #return
     df
   }
   else
   {
-    #return error
+    stop("Undefined or unsupported missing data mechanism.")
   }
-}
-
-##-------------------------------------------------------------------------------------------------------------------##
-
-replaceColumns <- function(original, toreplace)
-{
-  #Createa vector containing the names of the overlap between the replacement and the original data frame
-  change_columns <- colnames(original)[colnames(original) %in% colnames(toreplace)]
-  
-  #Now replace each column
-  for(i in 1:length(change_columns))
-  {
-    original[change_columns[i]] <- toreplace[change_columns[i]]
-  }
-  
-  #And return the merged data frame
-  return(original)
 }
