5776eab248912f071bf3892ed1738f4fba93e856
Nekomancer <Wufftiger@gmail.com>
2021-03-14 09:37:33 +0100

    Update run.R

--- run.R
+++ run.R
@@ -1,6 +1,8 @@
 ## Clean up the workspace
 rm(list=ls(all=TRUE))
 
+
+setwd("C:/Users/wufft/Desktop/RL Kram/Uni/Bachelor Thesis/R files/code/assignment_sim")
 ##Libraries
 library("SimDesign")
 
@@ -9,6 +11,11 @@ source("sim_functions.R")
 
 set.seed(103415)
 
-tmp <- simData(n=500, coefficients=coeff_list[[3]], r.sqrd=r.squared[1], parameters=parameters)
-tmp <- makeMissing(data=tmp, mechanism="MCAR", percent=.25)
-tmp
+
+start_time <- Sys.time()
+dat <- simData(n=500, coefficients=coeff_list[[1]], r.sqrd=r.squared[1], parameters=parameters)
+dat <- makeMissing(data=dat, mechanism="MCAR", percent=.25)
+end_time <- Sys.time()
+
+sum(is.na(dat))
+end_time - start_time
