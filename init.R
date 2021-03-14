8ce419571991ce1c43ec71d6f81981df1c03ce24
Nekomancer <Wufftiger@gmail.com>
2021-03-14 09:37:14 +0100

    Update init.R

--- init.R
+++ init.R
@@ -5,7 +5,8 @@ parameters <- list()
 M <- 10                                     # Number of iterations
 nvec <- seq(100,10000,100)                  # sample sizes
 r.squared <- c(.1, .33, .5, .66, .9)        # R-squared
-coeff_list <- list(c(1, .5, 3.3),
+coeff_list <- list(c(1, .5),
+                   c(1, .5, 3.3),
                    c(1, .5, 3.3, 2),
                    c(1, .5, 3.3, 2, 1.5))   # Coefficients for the predictor variables, also number of predictors (length -1)
 
