18411c1414b7fd809033bf75c0864def1cfa1f3a
Nekomancer <Wufftiger@gmail.com>
2021-03-19 16:54:38 +0100

    Update init.R

--- init.R
+++ init.R
@@ -1,16 +1,24 @@
 #Parameters of the simulation:
 parameters <- list()
 
+
+
 #Variable
-M <- 10                                     # Number of iterations
-nvec <- seq(100,10000,100)                  # sample sizes
-r.squared <- c(.1, .33, .5, .66, .9)        # R-squared
-coeff_list <- list(c(1, .5),
-                   c(1, .5, 3.3),
-                   c(1, .5, 3.3, 2),
-                   c(1, .5, 3.3, 2, 1.5))   # Coefficients for the predictor variables, also number of predictors (length -1)
+covariances <- c(0, 3.33, 10)       # Covariances
+r.squared   <- c(0, .1, .5)         # R-square
+mechanism   <- c("MCAR", "MAR")     # Missing data mechanism
+snr         <- c(.65, .75)          # SNR
+mtype       <- c(1,2)               # Matching type for PMM
+km          <- c(1, 3 ,5, 10)       # k for each match type
+method      <- c("MI", "PMM")
+
+
+iter <- 1                           # iterations
 
 #Fixed
-parameters$covariance <- c(.1)                              #covariances
-
+parameters$n    <- 500              #sample size
+parameters$miss <- .25              #percentage of missingness
+parameters$M    <- 10               #number of imputations
+parameters$c    <- list(c(1, 1.5),  #Coefficients for study 1
+                        c(0, 2))    #and 2 respectively
 
