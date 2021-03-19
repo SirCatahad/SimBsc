c638567484629b1b6c937532e0bcf1f97a6dd7a8
Nekomancer <Wufftiger@gmail.com>
2021-03-19 16:53:28 +0100

    Updated
    Rough outline of what it is supposed to look like. Data generation and missingness seem to work

--- run.R
+++ run.R
@@ -1,22 +1,109 @@
 ## Clean up the workspace
 rm(list=ls(all=TRUE))
 
-##Libraries
-library("SimDesign")
+
+## Libraries
+library("mice")
 
 source("init.R")
+source("simMissingness.R")
 source("sim_functions.R")
+
 
 set.seed(103415)
 
+#Data generation and storage
 
-start_time <- Sys.time()
-for(i in 1:1000)
+for(i in iter)
 {
-  dat <- simData(n=500, coefficients=coeff_list[[3]], r.sqrd=r.squared[1], parameters=parameters)
-  dat <- makeMissing(data=dat, mechanism="MCAR", percent=.25)
+  #Create a data table for each iteration containing all relevant information
+  table <- list()
+  for(cr in zip(covariances, r.squared))
+  {
+    #Generate data with given parameters
+    data <- try(with(parameters, simData(n    = n,
+                                 coefficients = c[[1]],
+                                 covariance   = cr[[1]],
+                                 r.sqrd       = cr[[2]])))
+    
+    
+    #Extract relevant information, coefficients, r.squared, etc.
+    #analyze(data)
+    
+    #Poke holes into the dataset
+    for(m in mechanism)
+    {
+      #If it's MCAR
+      if(m == "MCAR")
+      {
+        MCAR_out <- try(with(parameters, makeMissing(data      = data,
+                                                     mechanism = m,
+                                                     pm        = miss
+                                    )))
+        data_MCAR <- data
+        data_MCAR[MCAR_out$r,1] <- NA
+        
+        #extract relevant information from the set
+        #analyze(data_MCAR)
+        
+        # Impute
+        for(me in method)
+        {
+          if(me=="PMM")
+          {
+            for(match in mtype)
+            {
+              for(k in km)
+              {
+                
+              }
+            }
+          }
+          else
+          {
+            
+          }
+        }
+      }
+      #Otherwise it's MAR
+      else
+      {
+        data_MAR <- data
+        for(s in snr)
+        {
+          MAR_out <- try(with(parameters, makeMissing(data      = data,
+                                                      mechanism = m,
+                                                      pm        = miss,
+                                                      preds     = colnames(data),
+                                                      snr       = s
+                                                      )))
+          data_MAR <- data
+          data_MAR[MAR_out$r,1] <- NA
+          
+          #Impute
+          for(me in method)
+          {
+            if(me=="PMM")
+            {
+              for(match in mtype)
+              {
+                for(k in km)
+                {
+                  #Analyze and store relevant information
+                }
+              }
+            }
+            else
+            {
+              #analyze and store relevant information
+            }
+          }
+        }
+      }
+    }
+  }
 }
-end_time <- Sys.time()
 
-sum(is.na(dat))
-end_time - start_time
+plot(data)
+points(data_MCAR, col='white')
+points(data_MAR, col='white')
