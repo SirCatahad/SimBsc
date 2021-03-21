ef58beab4da4d6850ada1441d9f775f997294b65
Nekomancer <Wufftiger@gmail.com>
2021-03-21 11:46:06 +0100

    Update init.R

--- init.R
+++ init.R
@@ -7,13 +7,13 @@ parameters <- list()
 covariances <- c(0, 3.33, 10)       # Covariances
 r.squared   <- c(0, .1, .5)         # R-square
 mechanism   <- c("MCAR", "MAR")     # Missing data mechanism
-snr         <- c(.65, .75)          # SNR
+snr         <- c(.325, .6)          # SNR
 mtype       <- c(1,2)               # Matching type for PMM
 km          <- c(1, 3 ,5, 10)       # k for each match type
 method      <- c("MI", "PMM")
 
 
-iter <- 1                           # iterations
+iter <- 4                           # iterations
 
 #Fixed
 parameters$n    <- 500              #sample size
@@ -21,4 +21,6 @@ parameters$miss <- .25              #percentage of missingness
 parameters$M    <- 10               #number of imputations
 parameters$c    <- list(c(1, 1.5),  #Coefficients for study 1
                         c(0, 2))    #and 2 respectively
+
+
 
