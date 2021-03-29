68aac52d60853b355091b7c6ce85745f86673ac6
Nekomancer <Wufftiger@gmail.com>
2021-03-29 09:04:33 +0200

    Removed name overlap with run
    Also pre-initialized df instead of growing empty vector

--- auc_snr_test.R
+++ auc_snr_test.R
@@ -12,28 +12,36 @@ source("sim_functions.R")
 
 set.seed(103415)
 
-auc <- vector()
-
 M <- 100
+snr_seq <- seq(0.1, 1,0.01)
 
+auc_snr_df <- data.frame(matrix(0,nrow=M*length(snr_seq), ncol=2))
+colnames(auc_snr_df) <- c("auc", "snr")
+auc_snr_df["snr"] <- snr_seq
+
+
+
+cntr <- 1
 for(i in 1:M)
 {
-  for(snr in seq(0.1,1,0.01))
+  for(snr in snr_seq)
   {
     data <- try(with(parameters, simData(n    = n,
                                          coefficients = c[[1]],
                                          covariance   = 3.33,
                                          r.sqrd       = .1)))
-    MAR_out <- try(with(parameters, makeMissing(data      = data,
-                                                mechanism = "MAR",
-                                                pm        = miss,
-                                                preds     = colnames(data),
-                                                snr       = snr
+    MAR_out_test <- try(with(parameters, makeMissing(data      = data,
+                                                     mechanism = "MAR",
+                                                     pm        = miss,
+                                                     preds     = colnames(data),
+                                                     snr       = snr
     )))
-    auc <- c(auc, MAR_out$auc)
+    auc_snr_df[cntr,"auc"] <- MAR_out_test$auc
+    cntr <- cntr+1
   }
 }
-df <- data.frame(cbind(auc, seq(0.1, 1, .01)))
-colnames(df) <- c("auc", "snr")
-df <- df[order(df$snr),]
-plot(df)
+plot(auc_snr_df)
+abline(v=.65,  col='red')
+abline(h=.325, col='red')
+abline(v=.75,  col='blue')
+abline(h=.6,   col='blue')
