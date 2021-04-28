1be1016b0254bbcd5a558784d391b5196c7c18ae
Nekomancer <Wufftiger@gmail.com>
2021-04-28 19:40:15 +0200

    Update init.R

--- init.R
+++ init.R
@@ -9,16 +9,9 @@ r.squared   <- c(0, .1, .5)         # R-square
 snr         <- c(.325, .6)          # SNR
 mtype       <- c(1,2)               # Matching type for PMM
 km          <- c(1, 3 ,5, 10)       # k for each match type
-method      <- c("MI", "PMM")
-
-nya <- expand.grid(covariances,snr, km, mtype)
-
 iter <- 10                          # iterations
 
 #Fixed
 parameters$n    <- 500              #sample size
 parameters$miss <- .25              #percentage of missingness
 parameters$M    <- 10               #number of imputations
-
-
-
