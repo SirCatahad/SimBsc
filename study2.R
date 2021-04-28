f1a5cf3463a7b7b180f2aa711bbe092191aeb0c3
Nekomancer <Wufftiger@gmail.com>
2021-04-29 00:19:25 +0200

    Update study2.R

--- study2.R
+++ study2.R
@@ -15,6 +15,9 @@ set.seed(162551)
 #Will be a few Megabytes. Probably not a good idea if it gets larger
 storage_i <- vector(mode="list",iter)
 
+#Time it! 
+start_time <- Sys.time()
+
 ## Main Simulation loop -----------------------------------------------------------------------------------------------------
 #Study 1
 for(i in 1:iter)
@@ -23,15 +26,19 @@ for(i in 1:iter)
   storage_i[[i]] <- runIteration(covariances, parameters, km, mtype, snr, "study2", i)
   
   ##Save to disk every 50 iterations
-  if(i %% 50==0)
+  if(i %% 2==0)
   {
     saveRDS(storage_i[!sapply(storage_i,is.null)],
-            file = paste0("../data/study2/data_iteration_",i-49,"_",i,".rds")
+            file = paste0("../data/study2/data_iteration_",i-1,"_",i,".rds")
     )
     storage_i <- vector(mode="list",iter)
   }
   #  savetodisk
 }
+end_time <- Sys.time()
+
+end_time - start_time
+
 
 ## Read in all the files
 files <- list.files(path = "../data/study2/", pattern = "\\.rds$", full.names = TRUE)
