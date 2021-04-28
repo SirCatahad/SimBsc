c9585cf4da6197d9c8f880a9ad67cc1f24d9c797
Nekomancer <Wufftiger@gmail.com>
2021-04-29 00:49:52 +0200

    Update init.R

--- init.R
+++ init.R
@@ -9,7 +9,7 @@ r.squared   <- c(0, .1, .5)         # R-square
 snr         <- c(.325, .6)          # SNR
 mtype       <- c(1,2)               # Matching type for PMM
 km          <- c(1, 3 ,5, 10)       # k for each match type
-iter <- 4                       # iterations
+iter <- 1000                        # iterations
 
 #Fixed
 parameters$n    <- 500              #sample size
