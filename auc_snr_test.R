1ca4061d4b31cb310281a155cc4ba22bd82c833e
Nekomancer <Wufftiger@gmail.com>
2021-05-14 22:32:31 +0200

    Updated plot to be in line with APA guidelines

--- auc_snr_test.R
+++ auc_snr_test.R
@@ -5,6 +5,7 @@ rm(list=ls(all=TRUE))
 ## Libraries
 library("mice")
 
+
 source("init.R")
 source("simMissingness.R")
 source("sim_functions.R")
@@ -12,35 +13,43 @@ source("sim_functions.R")
 
 set.seed(103415)
 
-M <- 100
+test_iter <- 100
 snr_seq <- seq(0.1, 1,0.01)
 
-auc_snr_df <- data.frame(matrix(0,nrow=M*length(snr_seq), ncol=2))
+auc_snr_df <- data.frame(matrix(0,nrow=test_iter*length(snr_seq), ncol=2))
 colnames(auc_snr_df) <- c("auc", "snr")
 auc_snr_df["snr"] <- snr_seq
 
 
 
 cntr <- 1
-for(i in 1:M)
+for(i in 1:test_iter)
 {
+  data <- try(with(parameters, simData(n    = n,
+                                       beta   = 0)))
   for(snr in snr_seq)
   {
-    data <- try(with(parameters, simData(n    = n,
-                                         coefficients = c[[1]],
-                                         covariance   = 3.33,
-                                         r.sqrd       = .1)))
-    MAR_out_test <- try(with(parameters, makeMissing(data      = data,
-                                                     mechanism = "MAR",
-                                                     pm        = miss,
-                                                     preds     = colnames(data),
-                                                     snr       = snr
-    )))
+    
+    MAR_out_test <- try(with(parameters, simLinearMissingness(pm       = miss,
+                                                              data     = data,
+                                                              snr      = snr,
+                                                              type     = "high",
+                                                              optimize = FALSE)
+    ))
     auc_snr_df[cntr,"auc"] <- MAR_out_test$auc
     cntr <- cntr+1
   }
 }
-plot(auc_snr_df)
+windowsFonts(A = windowsFont("Times New Roman"))
+plot(auc_snr_df,
+     family="A",
+     xlab="AUC",
+     ylab="SNR",cex.lab=1.2, cex.axis=1.2, cex.main=1.2, cex.sub=1.2,
+     yaxt='n',
+     bty = "n",
+     axes=FALSE)
+axis(1, col="white", tcl=0)
+axis(2, col="white", tcl=0, las=1)
 abline(v=.65,  col='red')
 abline(h=.325, col='red')
 abline(v=.75,  col='blue')
