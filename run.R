887dc6a70784557d9ff07adbe7d523dd0625182f
Nekomancer <Wufftiger@gmail.com>
2021-05-14 22:33:53 +0200

    Update run.R

--- run.R
+++ run.R
@@ -1,5 +1,7 @@
 ## Clean up the workspace
 rm(list=ls(all=TRUE))
+setwd("C:/Users/wufft/Desktop/RL Kram/Uni/Bachelor Thesis/R files/code")
+#install.packages("SimDesign", repos = "http://cloud.r-project.org")
 
 ## Libraries
 library("mice")
@@ -12,29 +14,26 @@ source("sim_functions.R")
 set.seed(162551)
 
 #Stores the resulting data frame of each iteration so we can combine them later
-#Will be a few Megabytes. Probably not a good idea if it gets larger
-storage_i <- vector(mode="list",iter)
+storage_i <- vector(mode="list",50)
 
 ## Main Simulation loop -----------------------------------------------------------------------------------------------------
-                                        #Study 1
 for(i in 1:iter)
 {
-  ## Run one iteration
-  storage_i[[i]] <- runIteration(covariances, parameters, km, mtype, snr, "study1", i)
+  ## Run one iteration and save in list
+  storage_i[[((i-1)%%50)+1]] <- runIteration(betav, parameters, km, mtype, snr, study, i)
   
   ##Save to disk every 50 iterations
   if(i %% 50==0)
   {
     saveRDS(storage_i[!sapply(storage_i,is.null)],
-            file = paste0("../data/study1/data_iteration_",i-49,"_",i,".rds")
+            file = paste0(outputDir,"data_iteration_",i-49,"_",i,".rds") 
     )
-    storage_i <- vector(mode="list",iter) ### KML: If you're only going to use 50 slots at a time, why not make a length 50 list and compute the index with the modulo?
+    storage_i <- vector(mode="list",50)
   }
-  #  savetodisk
 }
 
 ## Read in all the files
-files <- list.files(path = "../data/study1/", pattern = "\\.rds$", full.names = TRUE)
+files <- list.files(path = outputDir, pattern = "\\.rds$", full.names = TRUE)
 df <- lapply(files, readRDS)
 
 ## We now have a list of lists of data frames
